ac58c5616e3a14712991a3f89eae5a6c7bd93f47
bobrock <barret.obrock@gmail.com>
2020-02-01 17:15:46 -0600

    Change Events API path

--- viktor/app.py
+++ viktor/app.py
@@ -17,7 +17,7 @@ Bot = Viktor(bot_name, key_dict['xoxb_token'], key_dict['xoxp_token'])
 app = Flask(__name__)
 
 # Events API listener
-bot_events = SlackEventAdapter(key_dict['signing_secret'], "/vikapi/events", app)
+bot_events = SlackEventAdapter(key_dict['signing_secret'], "/viktor/vikapi/events", app)
 
 
 @bot_events.on('reaction_added')
