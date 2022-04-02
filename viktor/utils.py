0274965ee23af47ce56f15af4d8c221d072207b5
bobrock <barret@tarkmail.com>
2022-04-01 20:05:56 -0500

    Repair pin collection utility

--- viktor/utils.py
+++ viktor/utils.py
@@ -1,30 +1,60 @@
 from datetime import datetime
-import pytz
 from typing import Dict
-from sqlalchemy.orm import Session
+import pytz
+from sqlalchemy.sql import (
+    func,
+    or_
+)
+from sqlalchemy.engine.row import Row
 from easylogger import Log
-from .model import TableUsers, TableQuotes
+from viktor.db_eng import ViktorPSQLClient
+from viktor.model import (
+    TableSlackChannel,
+    TableSlackUser,
+    TableQuote
+)
 
 
-def collect_pins(pin_dict: Dict, session: Session, log: Log) -> TableQuotes:
+def collect_pins(pin_dict: Dict, psql_client: ViktorPSQLClient, log: Log) -> TableQuote:
     """Attempts to load pinned message into the quotes db"""
+    us_ct = pytz.timezone('US/Central')
     if pin_dict.get('message') is None:
         # Receiving a pin message in-prod is different than when using the historical response from /api/pin_list
         pin_dict = pin_dict.get('item')
-    author_sid = pin_dict.get('message').get('user')
+    author_uid = pin_dict.get('message').get('user')
     author_name = pin_dict.get('message').get('username')
-    if author_sid is None:
+    if author_uid is None:
         # Try getting bot id
-        author_sid = pin_dict.get('message').get('bot_id')
-    author = session.query(TableUsers).filter(TableUsers.slack_id == author_sid).one_or_none()
-    if author is None:
-        # Most probably because it's slackbot. Add user to table
-        log.debug(f'Author not found in user table. Adding: {author_sid}')
-        author_name = 'UNKNOWN' if author_name is None else author_name
-        session.add(TableUsers(slack_id=author_sid, name=author_name))
-        session.commit()
-        author = session.query(TableUsers).filter(TableUsers.slack_id == author_sid).one_or_none()
-    pinner = session.query(TableUsers).filter(TableUsers.slack_id == pin_dict.get('created_by')).one_or_none()
+        author_uid = pin_dict.get('message').get('bot_id', '')
+
+    with psql_client.session_mgr() as session:
+        author = session.query(TableSlackUser).filter(or_(
+            TableSlackUser.slack_user_hash == author_uid,
+            TableSlackUser.slack_bot_hash == author_uid
+        )).one_or_none()
+
+        if author is None and author_uid.startswith('B') and author_name is not None:
+            # Probably a bot thing. Check if the user hash begins with 'b'.
+            #   If so, try to write that id to the db so next time it goes through
+            author = session.query(TableSlackUser).filter(
+                func.lower(TableSlackUser.real_name) == author_name.lower()
+            ).one_or_none()
+        if author is None:
+            # If author is still none, use the unknown user
+            author = session.query(TableSlackUser).filter(
+                TableSlackUser.slack_bot_hash == 'BUNKNOWN'
+            ).one_or_none()
+        session.expunge(author)
+
+    with psql_client.session_mgr() as session:
+        pinner = session.query(TableSlackUser).filter(
+            TableSlackUser.slack_user_hash == pin_dict.get('created_by')
+        ).one_or_none()
+        if pinner is None:
+            pinner = session.query(TableSlackUser).filter(
+                TableSlackUser.slack_bot_hash == 'BUNKNOWN'
+            ).one_or_none()
+        session.expunge(pinner)
     log.debug('Adding pinned message to table...')
 
     text = pin_dict.get('message').get('text')
@@ -34,16 +64,22 @@ def collect_pins(pin_dict: Dict, session: Session, log: Log) -> TableQuotes:
             text += f'\n{file.get("url_private")}'
     if text == '':
         # Try getting attachment info
-        for att in pin_dict.get('message').get('attachments'):
+        for att in pin_dict.get('message').get('attachments', []):
             text += att.get('image_url')
     log.debug(f'Passing text: "{text[:10]}"')
-    return TableQuotes(
-        author=author.id,
-        channel=pin_dict.get('channel'),
-        pinner=pinner.id,
+    with psql_client.session_mgr() as session:
+        channel_key = session.query(TableSlackChannel.channel_id).filter(
+            TableSlackChannel.slack_channel_hash == pin_dict.get('channel')
+        ).one_or_none()
+        if isinstance(channel_key, Row):
+            # Convert to id
+            channel_key = next(iter(channel_key), None)
+    return TableQuote(
         text=text,
+        author_user_key=author.user_id,
+        channel_key=channel_key,
+        pinner_user_key=pinner.user_id if pinner is not None else None,
         link=pin_dict.get('message').get('permalink'),
-        message_date=datetime.fromtimestamp(float(pin_dict.get('message').get('ts')),
-                                            pytz.timezone('US/Central')),
-        pin_date=datetime.fromtimestamp(pin_dict.get('created'), pytz.timezone('US/Central'))
+        message_timestamp=datetime.fromtimestamp(float(pin_dict.get('message').get('ts')), us_ct),
+        pin_timestamp=datetime.fromtimestamp(pin_dict.get('created'), us_ct)
     )
