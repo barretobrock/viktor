aa8d907e3e509083f637d3992aa60d3ddff858ba
bobrock <barret@tarkmail.com>
2022-04-18 09:45:53 -0500

    Fix logging inconsistency on crons.py

--- viktor/crons.py
+++ viktor/crons.py
@@ -16,13 +16,15 @@ from viktor.model import (
 )
 from viktor.core.emoji_scraper import scrape_emojis
 import viktor.app as mainapp
+from viktor.logg import get_base_logger
 
 
 cron = Blueprint('cron', __name__)
+logg = get_base_logger()
 
 
 @cron.route('/new-emojis', methods=['POST'])
-@mainapp.logg.catch
+@logg.catch
 def handle_cron_new_emojis():
     """Check for newly uploaded emojis (triggered by cron task that sends POST req every 60m mins)
 
@@ -53,7 +55,7 @@ def handle_cron_new_emojis():
 
 
 @cron.route('/new-potential-emojis', methods=['POST'])
-@mainapp.logg.catch
+@logg.catch
 def handle_cron_new_potential_emojis():
     """Daily check for new potential emojis (triggered by cron task that sends POST req every 10 mins)
 
@@ -92,7 +94,7 @@ def handle_cron_new_potential_emojis():
 
 
 @cron.route("/profile-update", methods=['POST'])
-@mainapp.logg.catch
+@logg.catch
 def handle_cron_profile_update():
     """Check for newly updated profile elements (triggered by cron task that sends POST req every 1 hr)"""
     # Check emojis uploaded (every 60 mins)
@@ -172,7 +174,7 @@ def handle_cron_profile_update():
 
 
 @cron.route("/reacts", methods=['POST'])
-@mainapp.logg.catch
+@logg.catch
 def handle_cron_reacts():
     """Check for new reactions - if any, run through and react to them"""
     pass
